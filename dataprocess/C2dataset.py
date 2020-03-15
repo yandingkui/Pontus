c18b8d5ded413c526dbdba97851ff2475bb0e151
yandingkui <945274875@qq.com>
2020-03-15 09:49:01 +0800

    匹配代码第一版

--- dataprocess/C2dataset.py
+++ dataprocess/C2dataset.py
@@ -3,16 +3,21 @@
 def filterData():
     count=0
     infectedIP=set()
+    alldata=list()
     with open("../result_data/match_dga") as f:
         for line in f:
             lines=line.strip().split(",")
+            alldata.append(lines)
             if lines[15]=="MULL":
                 count=count+1
                 infectedIP.add(lines[0])
-    print(count)
-    print(len(infectedIP))
-    for ip in infectedIP:
-        print(ip)
+    resultfile=open("../result_data/dga_A","a+")
+    for lines in alldata:
+        if (lines[0] in infectedIP) and (lines[15]!="MULL"):
+            resultfile.write(",".join(lines)+"\n")
+    resultfile.close()
+
+
 
 if __name__=="__main__":
     filterData()
