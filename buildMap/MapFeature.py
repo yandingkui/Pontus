3dbf311b13a0fa33fe3e178f4ecfd95cdf23912d
yandingkui <945274875@qq.com>
2020-03-21 13:04:53 +0800

    active domain进行字符串实验

--- buildMap/MapFeature.py
+++ buildMap/MapFeature.py
@@ -1,10 +1,36 @@
 import redis
 
-def getSingleFeature(domain):
-    redis_domain = redis.Redis(host='127.0.0.1', port=6379, db=1)
-    redis_ip = redis.Redis(host='127.0.0.1', port=6379, db=2)
-    redis_CNAME = redis.Redis(host='127.0.0.1', port=6379, db=3)
+def getFeature(domain):
+    redisDomainDB = redis.Redis(host='127.0.0.1', port=6379, db=1)
+    redisIPDB = redis.Redis(host='127.0.0.1', port=6379, db=2)
+    redisCNAMEDB = redis.Redis(host='127.0.0.1', port=6379, db=3)
+
+    ttl_map=dict()
+
+    with open("../result_data/dga_virustotal_A","r") as f:
+        for r in f:
+            line=r.strip()
+            lines=line.split(",")
+            domain=lines[3].strip()
+            ttl=lines[10].strip()
+            ttls=ttl_map.get(domain)
+            if ttls is None:
+                ttls=set()
+                ttls.add(ttl)
+                ttl_map[domain]=ttls
+            else:
+                ttls.add(ttl)
+    for k,v in ttl_map.items():
+        print("{}:{}".format(k,len(v)))
+
+    ipset=redisDomainDB.smembers(domain)
+    ipgetDomainset=[]
+    for ip in ipset:
+        ipgetDomainset.append(redisIPDB.hgetall())
 
 
+    cnameset=redisCNAMEDB.smembers(domain)
 
 
+if __name__=="__main__":
+    getFeature()
