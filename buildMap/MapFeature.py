615e4f562df9a65bdf89dcf0cfffae03134df1c0
yandingkui <945274875@qq.com>
2020-03-24 17:05:42 +0800

    active domain进行字符串实验

--- buildMap/MapFeature.py
+++ buildMap/MapFeature.py
@@ -44,43 +44,44 @@ def getFeature(domain,nowdate):
 
     vector=np.zeros(11)
     ipset=redisDomainDB.smembers(domain)
-    #域名解析IP的个数
-    vector[0]=len(ipset)
 
-    alldays=[]
-    weekdays=[]
-    priratios=[]
+    if (ipset is not None) and len(ipset)>0:
+        # 域名解析IP的个数
+        vector[0] = len(ipset)
+        alldays=[]
+        weekdays=[]
+        priratios=[]
 
-    #ip承载的
-    for ip in ipset:
-        #统计所有的域名数量
-        ip_domain_map=redisIPDB.hgetall(ip)
-        ipAllNum=len(ip_domain_map)
-        alldays.append(ipAllNum)
-        pri_map=dict()
+        #ip承载的
+        for ip in ipset:
+            #统计所有的域名数量
+            ip_domain_map=redisIPDB.hgetall(ip)
+            ipAllNum=len(ip_domain_map)
+            alldays.append(ipAllNum)
+            pri_map=dict()
 
-        weeknum=0
+            weeknum=0
 
-        for k,v in ip_domain_map.items():
-            domain_time_str=str(v)
-            vs=domain_time_str[domain_time_str.index("'")+1:domain_time_str.rindex("'")]
-            domain_name_str=str(k)
-            dns = domain_name_str[domain_name_str.index("'") + 1:domain_name_str.rindex("'")]
+            for k,v in ip_domain_map.items():
+                domain_time_str=str(v)
+                vs=domain_time_str[domain_time_str.index("'")+1:domain_time_str.rindex("'")]
+                domain_name_str=str(k)
+                dns = domain_name_str[domain_name_str.index("'") + 1:domain_name_str.rindex("'")]
 
-            domain_time=datetime.datetime.strptime(vs,'%Y%m%d%H%M%S')
-            #如果该域名在一个星期之内
-            if domain_time>beforeWeekDate:
-                weeknum=weeknum+1
-            #统计子域名个数
-            domain_pri=psl.privatesuffix(dns)
-            pri_num=pri_map.get(domain_pri)
-            if pri_num is None:
-                pri_map[domain_pri]=1
-            else:
-                pri_map[domain_pri]=pri_num+1
+                domain_time=datetime.datetime.strptime(vs,'%Y%m%d%H%M%S')
+                #如果该域名在一个星期之内
+                if domain_time>beforeWeekDate:
+                    weeknum=weeknum+1
+                #统计子域名个数
+                domain_pri=psl.privatesuffix(dns)
+                pri_num=pri_map.get(domain_pri)
+                if pri_num is None:
+                    pri_map[domain_pri]=1
+                else:
+                    pri_map[domain_pri]=pri_num+1
 
-        weekdays.append(weeknum)
-        priratios.append(max(pri_map.values())/ipAllNum)
+            weekdays.append(weeknum)
+            priratios.append(max(pri_map.values())/ipAllNum)
 
 
     #解析IP一周内的解析情况
@@ -114,7 +115,9 @@ if __name__=="__main__":
             if items[1].strip() == "True":
                 AGDs.append(items[0].strip())
     for d in AGDs:
-        v=getFeature("www.baidu.com",datetime.datetime.strptime("20180507",'%Y%m%d'))
-        print(v)
+        try:
+            v=getFeature(d,datetime.datetime.strptime("20180507",'%Y%m%d'))
+        except:
+            print(d)
 
 
